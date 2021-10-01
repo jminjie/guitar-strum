3b379d31da5569e71752c6cf17e83faaefb1a30c
Jordan <jminjie@umich.edu>
2021-09-30 18:54:42 -0700

    Add vocals

--- main.js
+++ main.js
@@ -8,64 +8,189 @@ resize();
 function resize() {
     ctx.canvas.width  = document.body.scrollWidth;
     // total height of strings
-    ctx.canvas.height = 263;
+    ctx.canvas.height = 300;
 }
 
 // x position of the start of the string
 STRINGS_X = [512, 506, 499, 495, 500, 508];
 // y position of the string
-STRINGS_Y = [20, 66, 112, 158, 204, 250];
+STRINGS_Y = [35, 81, 127, 173, 219, 265];
 
 // y positions of user scroll to trigger the first strum
 SCROLL_TRIGGERS_Y = [500, 557, 557+45, 557+45+45, 557+45+45+45, 557+45+45+45+45];
 
-strum = [0, 0, 0, 0, 0, 0]
-STRING_NOTE = ["E1", "A1", "D2", "G2", "B3", "E4"];
-strum_x = [0, 0, 0, 0, 0, 0]
+GUITAR_VELOCITY = 0.1;
 
-const sampler = new Tone.Sampler({
+STRING_DECAY = 0.1;
+
+pluck = [0, 0, 0, 0, 0, 0]
+
+OPEN = ["E2", "A2", "D3", "G3", "B3", "E4"];
+
+G_CHORD = ["G2", "D3", "G3", "B3", "D4", "G4"];
+D_CHORD = ["D2", "A2", "D3", "F#3", "A3", "D4"];
+E_MIN_CHORD = ["E2", "B2", "E3", "G3", "B3", "E4"];
+C_CHORD = ["C3", "E3", "G3", "C4", "E4", "G4"];
+B_MIN_CHORD = ["B2", "D3", "F#3", "B3", "D4", "F#4"],
+
+STRING_NOTES = [ OPEN,
+    G_CHORD,
+    D_CHORD,
+    E_MIN_CHORD,
+    C_CHORD,
+    G_CHORD,
+    D_CHORD,
+    B_MIN_CHORD,
+    C_CHORD,
+    B_MIN_CHORD,
+    C_CHORD,
+    D_CHORD,
+    G_CHORD,
+    OPEN
+              ]
+pluck_x = [0, 0, 0, 0, 0, 0]
+
+const LINES = [
+'line0.mp3',
+'line1.mp3',
+'line2.mp3',
+'line3.mp3',
+'line4.mp3',
+'line5.mp3',
+'line6.mp3',
+'line7.mp3',
+'silent.mp3',
+'line8.mp3',
+'silent.mp3',
+'line9.mp3',
+'silent.mp3',
+];
+
+var player = new Tone.Player().toDestination();
+
+const sampler1 = new Tone.Sampler({
     urls: {
-        "C3": "C3.wav",
-        "C4": "C4.wav",
-        "C5": "C5.wav",
+            "E2": "E2.mp3",
     },
-    release: 1,
-    baseUrl: "https://jminjie.github.io/samples/banjo/",
+    baseUrl: "https://jminjie.github.io/samples/guitar/",
 }).toDestination();
+const sampler2 = new Tone.Sampler({
+    urls: {
+            "A2": "A2.mp3",
+    },
+    baseUrl: "https://jminjie.github.io/samples/guitar/",
+}).toDestination();
+const sampler3 = new Tone.Sampler({
+    urls: {
+            "D3": "D3.mp3",
+    },
+    baseUrl: "https://jminjie.github.io/samples/guitar/",
+}).toDestination();
+const sampler4 = new Tone.Sampler({
+    urls: {
+            "G3": "G3.mp3",
+    },
+    baseUrl: "https://jminjie.github.io/samples/guitar/",
+}).toDestination();
+const sampler5 = new Tone.Sampler({
+    urls: {
+            "B3": "B4.mp3",
+    },
+    baseUrl: "https://jminjie.github.io/samples/guitar/",
+}).toDestination();
+const sampler6 = new Tone.Sampler({
+    urls: {
+            "E4": "E4.mp3",
+    },
+    baseUrl: "https://jminjie.github.io/samples/guitar/",
+}).toDestination();
+
+const samplers = [sampler1, sampler2, sampler3, sampler4, sampler5, sampler6];
 
 animate();
 
+var currentlyStrumming = false;
 var flip = [1, 1, 1, 1, 1, 1];
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.lineWidth = 5;
     ctx.strokeStyle = 'white';
 
+    let allStrumming = true;
     for (let i = 0; i < 6; i++) {
-        if (Math.abs(strum[i]) > 1) {
+        if (Math.abs(pluck[i]) > 1) {
             ctx.beginPath();
             ctx.moveTo(STRINGS_X[i], STRINGS_Y[i]);
-            ctx.bezierCurveTo(strum_x[i] ,STRINGS_Y[i] + strum[i], strum_x[i]+50,STRINGS_Y[i] - strum[i], 5000,STRINGS_Y[i]);
+            ctx.bezierCurveTo(pluck_x[i] ,STRINGS_Y[i] + pluck[i], pluck_x[i]+50,STRINGS_Y[i] - pluck[i], 5000,STRINGS_Y[i]);
             ctx.stroke();
-            //strum[i] = -Math.abs(strum[i]/1.15);
-            strum[i] = flip[i] * (Math.abs(strum[i]) - 0.2);
+            pluck[i] = flip[i] * (Math.abs(pluck[i]) - STRING_DECAY);
             flip[i] *= -1;
         } else {
-            strum[i] = 0;
+            allStrumming = false;
+            pluck[i] = 0;
             ctx.beginPath();
             ctx.moveTo(STRINGS_X[i], STRINGS_Y[i]);
             ctx.lineTo(5000, STRINGS_Y[i])
             ctx.stroke();
         }
     }
+
+    if (allStrumming) {
+        if (!currentlyStrumming) {
+            advanceChord();
+            playLineAndAdvance();
+        }
+        currentlyStrumming = true;
+    } else {
+        currentlyStrumming = false;
+    }
+
     requestAnimationFrame(animate);
 }
 
-function startStrum(i, x) {
-    sampler.triggerAttackRelease(STRING_NOTE[i], Tone.context.currentTime);
-    // 15 pix strum displacement
-    strum[i] = 15;
-    strum_x[i] = x;
+var chord_index = 0;
+function advanceChord() {
+    if (chord_index + 1 < STRING_NOTES.length) {
+        chord_index = chord_index + 1;
+    }
+}
+
+Tone.Transport.start();
+var line_index = 0;
+player.load(LINES[line_index]);
+function playLineAndAdvance() {
+    player.start();
+    if (line_index < LINES.length - 1) {
+        line_index += 1;
+        player.load(LINES[line_index]);
+    }
+
+    if (chord_index == 8 || chord_index == 10) {
+        // autoplay on chord 8 or 10
+        for (let i = 0; i < 6; i++) {
+            let DELAY_S = 1.3;
+            let note = STRING_NOTES[chord_index][i];
+            let time =  '+' + (DELAY_S + (i/14));
+            Tone.Transport.schedule((time) => {
+                samplers[i].triggerAttack(note, Tone.context.currentTime, GUITAR_VELOCITY);
+                startPluck(i, 1000);
+                // advance chord automatically on 6th note
+                if (i == 5) {
+                    advanceChord();
+                    playLineAndAdvance();
+                }
+            }, time);
+        }
+    }
+}
+
+function startPluck(i, x) {
+    samplers[i].releaseAll();
+    let note = STRING_NOTES[chord_index][i];
+    samplers[i].triggerAttack(note, Tone.context.currentTime, GUITAR_VELOCITY);
+    // 15 pix pluck displacement
+    pluck[i] = 15;
+    pluck_x[i] = x;
 }
 
 function getMousePos(evt) {
@@ -77,15 +202,15 @@ function getMousePos(evt) {
 }
 
 var lastScrollY = 0;
-window.addEventListener("scroll", scrollStrum);
+window.addEventListener("scroll", scrollPluck);
 
-function scrollStrum(event) {
+function scrollPluck(event) {
     for (let i = 0; i < 6; i++) {
         if (Math.sign(lastScrollY - SCROLL_TRIGGERS_Y[i]) !=
                 Math.sign(window.scrollY - SCROLL_TRIGGERS_Y[i])) {
-            // when triggering strum from scroll, just imagine that user mouse
+            // when triggering pluck from scroll, just imagine that user mouse
             // is around 1000 (middle of the string)
-            startStrum(i, 1000);
+            startPluck(i, 1000);
         }
     }
     lastScrollY = window.scrollY;
@@ -103,8 +228,8 @@ function pluckStrings(event) {
         for (let i = 0; i < 6; i++) {
             if (Math.sign(lastMouseY - STRINGS_Y[i]) != Math.sign(mouseY - STRINGS_Y[i])) {
                 // when user starts plucking manually, don't trigger by scroll anymore
-                window.removeEventListener("scroll", scrollStrum);
-                startStrum(i, mouseX);
+                window.removeEventListener("scroll", scrollPluck);
+                startPluck(i, mouseX);
             }
         }
     }
